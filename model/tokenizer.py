7149c4afba47124bae5d362fe6ad97ae8f61bc7c
Logan Reine <log.a.of.n@gmail.com>
2025-10-06 15:43:11 -0400

    finished the tokenizer, and created/saved a byte level vocab

--- model/tokenizer.py
+++ model/tokenizer.py
@@ -8,10 +8,16 @@ class Token:
     _ROOT = _DIR.parent
     _DATA_DIR = _ROOT / 'data' / 'structured'
     _VOCAB_DIR = _DIR / 'vocab'
-    _ALLOWED_TEXT = set(string.ascii_letters + string.digits + '!@#$%^&*()-_')
+    _ALLOWED_TEXT = sorted(set(string.ascii_letters + string.digits + '!@#$%^&*()-_'))
 
     def __init__(self):
-        pass
+        # hex / byte mappings
+        self._htoi = {f'{i:02x}': i for i in range(256)}
+        self._itoh = {i: f'{i:02x}' for i in range(256)}
+
+        # character / byte mappings
+        self._stoi = {ch: i for i,ch in enumerate(self._ALLOWED_TEXT)}
+        self._itos = {i: ch for ch, i in self._stoi.items()}
 
     @property
     def root(self):
@@ -20,39 +26,76 @@ class Token:
     @property
     def where(self):
         return str(self._DIR)
-    
-    def save_vocab(self, vocab: dict, fname: str, save_to: str | Path = ''):
-        import sys
-        sys.path.append(str(self._ROOT / 'util'))
-        from fileio import FileIO
 
-        f = self._VOCAB_DIR / fname if not save_to else Path(save_to / fname)
-        FileIO.save_json(vocab, path = f)
+    @property
+    def htoi(self) -> dict[str, int]:
+        '''hexadecimal string → byte value'''
+        return self._htoi
+
+    @property
+    def itoh(self) -> dict[int, str]:
+        '''byte value → hexadecimal string'''
+        return self._itoh
+
+    @property
+    def stoi(self) -> dict[str, int]:
+        '''character → integer index'''
+        return self._stoi
+
+    @property
+    def itos(self) -> dict[int, str]:
+        '''integer index → character'''
+        return self._itos
 
 
-    def load_corpus(self):
-        pass
+    def encode_hex(self, h: str) -> list[int]:
+        return [self._htoi[h[i : i + 2]] for i in range(0, len(h), 2)]
 
 
-    def build_hex_vocab(self) -> dict[str, int]:
-        hex_vocab = {f'{i:02x}': i for i in range(256)}
-        return hex_vocab
+    def decode_hex(self, encoding: list[int]) -> str:
+        return ''.join(self._itoh[i] for i in encoding)
+
+
+    def encode_txt(self, s: str) -> list[int]:
+        return [self._stoi[ch] for ch in s]
+
+
+    def decode_txt(self, encoding: list[int]) -> str:
+        return ''.join(self._itos[i] for i in encoding)
 
 
     def build_text_vocab(self):
         pass
 
 
-    def _decode_hex(self):
-        pass
+    def save_vocab(self, fname: str, save_to: str | Path = '') -> None:
+        import sys
+        sys.path.append(str(self._ROOT))
+        from util.fileio import FileIO
 
+        vocab = {
+            'htoi': self._htoi,
+            'itoh': self._itoh,
+            'stoi': self._stoi,
+            'itos': self._itos,
+        }
 
-    def encode(self):
-        pass
+        f = self._VOCAB_DIR / fname if not save_to else Path(save_to) / fname
+        FileIO.save_json(vocab, path = f)
 
+    
+    def load_vocab(self, path: str | Path) -> dict:
+        import sys
+        sys.path.append(str(self._ROOT))
+        from util.fileio import FileIO
 
-    def decode(self):
-        pass
+        vocab = FileIO.load_json(path)
 
+        # re-cast integer-keyed maps
+        self._itoh = vocab['itoh'] = {int(k): v for k, v in vocab['itoh'].items()}
+        self._itos = vocab['itos'] = {int(k): v for k, v in vocab['itos'].items()}
 
+        self._htoi = vocab['htoi']
+        self._stoi = vocab['stoi']
 
+        return vocab
